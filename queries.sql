af5e68dcdbb14dff7a97e325e05cc12d988cde36
Ishita282 <little.mini.ishita@gmail.com>
2024-09-13 19:37:36 +0530

    added all

---
+++ queries.sql
@@ -0,0 +1,23 @@
+//for retrieve all the movies, theaters, etc.
+
+select * from Movies;
+select * from Theaters;
+select * from Seats;
+select * from Showtime;
+select * from Screens;
+SELECT * FROM Users;
+
+
+// Retrieve All Shows for a Given Date and Theatre
+
+
+
+//Retrieve All Shows for a Given Movie
+
+
+
+// Retrieve All Shows in a Given Theatre
+
+
+
+// Retrieve Show Details for a Specific Show
